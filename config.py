e53af93800ddf0109d15cec7f8114a1fdb759de2
migueltrujillo07 <miguetrulo07@gmail.com>
2022-02-15 23:36:11 -0600

    Kata numero 9

---
+++ config.py
@@ -0,0 +1,5 @@
+def water_left(astronauts, water_left, days_left):
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
